7409cb4aa274e1367c6643a0cead8baf8bdd6eaa
Jonathan Schneider <jkschneider@gmail.com>
2023-01-21 10:37:31 -0500

    Explicit dependency on rewrite-core

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     // NOTE: this is latest.integration because we need to be able to release
     // rewrite-gradle-tooling-model BEFORE rewrite but also need to depend on
     // changes to the ABI of rewrite-maven.
+    compileOnly("org.openrewrite:rewrite-core:latest.integration")
     compileOnly("org.openrewrite:rewrite-maven:latest.integration")
     testRuntimeOnly("org.openrewrite:rewrite-maven:latest.integration")
 }
