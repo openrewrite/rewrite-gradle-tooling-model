502e3adf5d23e1e6ed5af611fb1849548cd27472
Sam Snyder <sam@moderne.io>
2023-09-29 14:15:51 -0700

    First attempt at a round trip serialization

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -11,4 +11,8 @@ dependencies {
     compileOnly("org.openrewrite:rewrite-core:latest.integration")
     compileOnly("org.openrewrite:rewrite-maven:latest.integration")
     testRuntimeOnly("org.openrewrite:rewrite-maven:latest.integration")
+    testImplementation(platform("com.fasterxml.jackson:jackson-bom:latest.release"))
+    testImplementation("com.fasterxml.jackson:jackson-core")
+    testImplementation("com.fasterxml.jackson:jackson-databind")
+    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-smile")
 }
---
+++ model/src/test/java/org/openrewrite/gradle/toolingapi/GradleProjectBuilderTest.java
@@ -0,0 +1,80 @@
+package org.openrewrite.gradle.toolingapi;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.CoercionAction;
+import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
+import com.fasterxml.jackson.databind.cfg.ConstructorDetector;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.type.LogicalType;
+import com.fasterxml.jackson.dataformat.smile.SmileFactory;
+import com.fasterxml.jackson.dataformat.smile.SmileGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GradleProjectBuilderTest {
+
+    @Test
+    void serializable(@TempDir Path dir) throws Exception {
+        try(InputStream is = GradleProject.class.getResourceAsStream("/build.gradle")) {
+            Files.write(dir.resolve("build.gradle"), Objects.requireNonNull(is).readAllBytes());
+        }
+        try(InputStream is = GradleProjectBuilderTest.class.getResourceAsStream("/settings.gradle")) {
+            Files.write(dir.resolve("settings.gradle"), Objects.requireNonNull(is).readAllBytes());
+        }
+
+        OpenRewriteModel model = OpenRewriteModelBuilder.forProjectDirectory(dir.toFile(), dir.resolve("build.gradle").toFile());
+        GradleProject gp = model.gradleProject();
+        assertThat(gp.getMavenRepositories()).isNotEmpty();
+        assertThat(gp.getNameToConfiguration()).isNotEmpty();
+        ObjectMapper m = buildMapper();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        m.writeValue(baos, model.gradleProject());
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        GradleProject roundTripped = m.readValue(bais, GradleProject.class);
+        assertThat(roundTripped).isEqualTo(gp);
+    }
+
+    ObjectMapper buildMapper() {
+        SmileFactory f = SmileFactory.builder()
+          .enable(SmileGenerator.Feature.CHECK_SHARED_STRING_VALUES)
+          .build();
+        JsonMapper.Builder mBuilder = JsonMapper.builder(f);
+
+        ObjectMapper m = mBuilder
+          // to be able to construct classes that have @Data and a single field
+          // see https://cowtowncoder.medium.com/jackson-2-12-most-wanted-3-5-246624e2d3d0
+          .constructorDetector(ConstructorDetector.USE_PROPERTIES_BASED)
+          .configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true)
+          .disable(MapperFeature.REQUIRE_TYPE_ID_FOR_SUBTYPES)
+          .build()
+          .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+          .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET)
+          .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        // workaround for problems with JavaType.Method#defaultValue (should be temporary)
+        m.coercionConfigFor(LogicalType.Collection)
+          .setCoercion(CoercionInputShape.EmptyString, CoercionAction.AsNull)
+          .setAcceptBlankAsEmpty(true);
+
+        return m.setVisibility(m.getSerializationConfig().getDefaultVisibilityChecker()
+          .withCreatorVisibility(JsonAutoDetect.Visibility.PUBLIC_ONLY)
+          .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+          .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE)
+          .withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+    }
+}
